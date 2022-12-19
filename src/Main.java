e5aad6ac16533ed60ab08a976cc8f3894e5d9d12
ZinaidaTian <112168681+ZinaidaTian@users.noreply.github.com>
2022-12-19 20:04:52 +0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
-        int eaters = 10; // сколько людей будут есть
+        int eaters = 5; // Г±ГЄГ®Г«ГјГЄГ® Г«ГѕГ¤ГҐГ© ГЎГіГ¤ГіГІ ГҐГ±ГІГј
 
-        int water = 3000; // миллилитров воды
-        int potatoes = 5; // картофелин
-        int chicken = 6; // куриных бёдер
-        int spices = 10; // ложек специй
+        int water = 3000; // Г¬ГЁГ«Г«ГЁГ«ГЁГІГ°Г®Гў ГўГ®Г¤Г»
+        int potatoes = 5; // ГЄГ Г°ГІГ®ГґГҐГ«ГЁГ­
+        int chicken = 6; // ГЄГіГ°ГЁГ­Г»Гµ ГЎВёГ¤ГҐГ°
+        int spices = 10; // Г«Г®Г¦ГҐГЄ Г±ГЇГҐГ¶ГЁГ©
 
-        System.out.println("Сварили суп. На одного человека вышло:");
-        System.out.println((water / eaters) + " миллилитров воды");
-        System.out.println((potatoes / eaters) + " картофелин(а)");
-        System.out.println((chicken / eaters) + " куриных(ое) бёдер(ро)");
-        System.out.println((spices / eaters) + " ложек(ка) специй");
+        System.out.println("Г‘ГўГ Г°ГЁГ«ГЁ Г±ГіГЇ. ГЌГ  Г®Г¤Г­Г®ГЈГ® Г·ГҐГ«Г®ГўГҐГЄГ  ГўГ»ГёГ«Г®:");
+        System.out.println((water / eaters) + " Г¬ГЁГ«Г«ГЁГ«ГЁГІГ°Г®Гў ГўГ®Г¤Г»");
+        System.out.println((potatoes / eaters) + " ГЄГ Г°ГІГ®ГґГҐГ«ГЁГ­(Г )");
+        System.out.println((chicken / eaters) + " ГЄГіГ°ГЁГ­Г»Гµ(Г®ГҐ) ГЎВёГ¤ГҐГ°(Г°Г®)");
+        System.out.println((spices / eaters) + " Г«Г®Г¦ГҐГЄ(ГЄГ ) Г±ГЇГҐГ¶ГЁГ©");
 
     }
 }
